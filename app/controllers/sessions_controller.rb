3478b6f341f74879a772f2e4856cf0a38fba970c
goodjobbin85 <td1729@hotmail.com>
2019-04-27 17:48:57 +0000

    sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Hello #{user.username}!"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = "Sorry. Please enter valid credentials."
+      render :new
+    end
   end
 
   def destroy
