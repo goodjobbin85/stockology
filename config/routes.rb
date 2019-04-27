2920707efa2cd347b0a746df5dacf25e41024655
goodjobbin85 <td1729@hotmail.com>
2019-04-27 03:05:47 +0000

    new user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'signup', to: "users#new"
+  get 'login', to: "sessions#new", as: "login"
+  post 'login', to: "sessions#create" 
+  get 'logout', to: "sessions#destroy", as: "logout"
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: "welcome#index"
   get 'search_users', to: "users#search"
   resources :users
