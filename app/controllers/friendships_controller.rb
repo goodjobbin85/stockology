8792187058425e5ecfa574179ceb1298dc7b7313
goodjobbin85 <td1729@hotmail.com>
2019-09-15 16:35:47 +0000

    friendship destroy action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,5 +4,13 @@ class FriendshipsController < ApplicationController
     end 
     
     def destroy
+        @friendship = current_user.friendships.where(friend_id: params[:id]).first
+        if @friendship.destroy
+            flash[:success] = "User succesfully removed from friends"
+            redirect_to my_friends_path
+        else
+            flash[:danger] = "Sorry, something went wrong."
+            redirect_to my_friends_path
+        end
     end
 end
