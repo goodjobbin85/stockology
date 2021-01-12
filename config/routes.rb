4dec37955412907621134ebc45a258efdaf6b358
Thomas Dubiel <td1729@hotmail.com>
2021-01-11 23:06:06 -0600

    start chatroom actioncable feature

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get 'my_friends', to: "users#my_friends"
   get 'search_stocks', to: "stocks#search"
   get 'portfolio', to: "users#portfolio"
-  post 'add_friend', to: "users#add_friend"
+  post 'add_friend', to: "users#add_friend" 
+
+  mount ActionCable.server, at: '/cable'
   
 end
