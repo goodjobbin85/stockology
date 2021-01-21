e19892cf8b2a27bc2cf23ce59c3837d81a39f38c
Thomas Dubiel <td1729@hotmail.com>
2021-01-21 00:04:00 -0600

    stock controller search method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,21 @@
 class StocksController < ApplicationController
 
-    def search
-        if params[:stock].blank? #what is the diff between .blank? and .present?
-            flash.now[:danger] = "Stock cannot be blank"
-            render 'users/portfolio'
-        else
-            @stock = Stock.find_stock(params[:stock])
-            flash.now[:danger] = "Please enter valid stock" unless @stock 
+    def search 
+        #need to fix flash alerts -- only appearing after redirect 
+        if params[:stock].present?
+            @stock = Stock.find_stock(params[:stock]) 
             if @stock 
                 respond_to do |format| 
-                    format.js { render partial: 'users/stock_result' }
-                end 
-            end 
-        end
-        
+                    format.js { render partial: 'users/stock_result' } 
+                    flash[:success] = "#{@stock.name} found!"
+                end  
+            else 
+                   flash[:danger] = "Please enter valid ticker symbol" 
+                   redirect_to portfolio_path
+            end  
+        else 
+            flash[:alert] = "Stock name cannot be blank!" 
+            redirect_to portfolio_path
+        end 
     end
 end
