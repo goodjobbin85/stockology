c9bc7e18e63074e2edc70c85826286b134fe015e
Thomas Dubiel <td1729@hotmail.com>
2021-03-09 14:15:40 -0600

    change actionmailer username

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
+  :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_API_KEY'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
