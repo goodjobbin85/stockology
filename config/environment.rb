a54cc2205cc2d9651af4efc507c6f1f6078efaa4
Thomas Dubiel <td1729@hotmail.com>
2021-03-09 11:35:06 -0600

    fix pg password to user env variables

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize! 
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
