38ec8647c7815cb696b9d4617973e35e1b36aef3
Thomas Dubiel <td1729@hotmail.com>
2021-03-09 14:00:06 -0600

    fix syntax error in apikey

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '<%= ENV['SENDGRID_API_KEY'] %>',
+  :password => '<SENDGRID_API_KEY>',
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
