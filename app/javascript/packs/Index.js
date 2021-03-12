da4cd1318a7554f2e37f5eafd3449ed460e5e64e
Thomas Dubiel <td1729@hotmail.com>
2021-03-12 06:03:08 -0600

    start Cart component

---
+++ app/javascript/packs/Index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { render } from "react-dom";
+import Cart from "../components/Cart" 
+
+document.addEventListener("DOMContentLoaded", () => { 
+    render( 
+        <Cart />
+    )
+});
