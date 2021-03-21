a6a58fd5576c5754255fab7ed54c40c65a52dcd5
Thomas Dubiel <td1729@hotmail.com>
2021-03-20 21:16:54 -0500

    generate cart resouce

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210302041303) do
+ActiveRecord::Schema.define(version: 20210321020902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "user_id"
