9bc21576922ff61cb00b42a9666a63813c837116
Thomas Dubiel <td1729@hotmail.com>
2021-04-09 21:40:15 -0500

    remove unnecessary remember methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,23 +3,10 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
-  
-    def forget(user)
-        user.forget 
-        cookies.delete(:user_id)
-        cookies.delete(:remember_token)
-    end 
     
     def log_out
-      forget(current_user)
       session[:user_id] = nil
       @current_user = nil
-    end
-    
-    def remember(user)
-        user.remember
-        cookies.permanent.signed[:user_id] = user.id
-        cookies.permanent[:remember_token] = user.remember_token
     end
     
     def logged_in?
@@ -27,16 +14,6 @@ module SessionsHelper
     end
     
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
-        elsif cookies.signed[:user_id]
-            user = User.find_by(id: cookies.signed[:user_id])
-            if user && user.authenticated?(cookies[:remember_token])
-                log_in(user)
-                @current_user = user 
-            end
-        end
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
-    
-    
 end
