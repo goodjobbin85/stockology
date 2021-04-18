8c3a7bbb18552eb89212ed4b47de640dc9b6a637
Thomas Dubiel <td1729@hotmail.com>
2021-04-17 21:33:21 -0500

    set_cart helper method

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,12 @@
-module CartsHelper
+module CartsHelper 
+    private 
+
+    def set_cart 
+        if @cart 
+            @cart = Cart.find(session[:cart_id]) 
+        else 
+            @cart = Cart.create 
+            session[:cart_id] = @cart.id 
+        end 
+    end 
 end
