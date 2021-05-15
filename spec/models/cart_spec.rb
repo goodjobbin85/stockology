a7f3da58f933fa522f1aab8fbafca2ae4e2231ed
Thomas Dubiel <td1729@hotmail.com>
2021-05-14 22:40:39 -0500

    update cart unit test

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to a user" do 
+
+  end 
+
+  it "has many line_items" do 
+
+  end 
+
+  
 end
